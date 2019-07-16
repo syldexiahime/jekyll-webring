2bd1a8b55ae3179c806f88b83449f2acb7dcf8b6
Sophie Askew <sophie@ofthewi.red>
2019-07-16 20:42:29 +0000

    add plugin config and use template thingie for generating webring

--- jekyll-webring.gemspec
+++ jekyll-webring.gemspec
@@ -20,7 +20,6 @@ Gem::Specification.new do |spec|
 	spec.required_ruby_version = ">= 2.3.0"
 	
 	spec.add_runtime_dependency "jekyll"
-	spec.add_runtime_dependency "abbreviato"
 	spec.add_runtime_dependency "sanitize"
 	
 	spec.add_development_dependency "bundler"
--- lib/jekyll-webring.rb
+++ lib/jekyll-webring.rb
@@ -2,29 +2,60 @@
 
 require 'rss'
 require 'open-uri'
-require 'abbreviato'
 require 'sanitize'
 
 module Jekyll
 	module Webring
-		FeedItem = Struct.new(:source_title, :source_url, :title, :url, :date, :summary)
+		TEMPLATE = <<~HTML
+			<section class="webring">
+				<h3>Articles from blogs I follow around the net</h3>
+				<section class="articles">
+					{% for item in webring %}
+					<div class="article">
+						<h4 class="title">
+							<a href="{{ item.url }}" target="_blank" rel="noopener">{{ item.title }}</a>
+						</h4>
+						<p class="summary">{{ item.summary }}</p>
+						<small class="source">
+							via <a href="{{ item.source_url }}">{{ item.source_title }}</a>
+						</small>
+						<small class="date">{{ item.date | date '%-d %B, %Y' }}</small>
+					</div>
+					{% endfor %}
+				</section>
+			</section>
+		HTML
+
+		CONFIG = Jekyll.configuration({})['webring']
+
+		@max_summary_length = 256
 
 		@feeds = [];
-		@url = 'https://christine.website/blog.rss'
-		@max = 256
-
 		def self.feeds ()
 			if @feeds.empty?
-				open @url do |rss|
-					feed = []
-					raw_feed = RSS::Parser.parse rss
-					raw_feed.items.each do |item|
-						sanitized = Sanitize.fragment item.content_encoded, Sanitize::Config::BASIC
-						summary, _ = Abbreviato.truncate sanitized, max_length: @max
-						feed << FeedItem.new(raw_feed.channel.title, raw_feed.channel.link,
-								item.title, item.link, item.pubDate, summary)
+				urls = CONFIG['feeds']
+
+				urls.each do |url|
+					open url do |rss|
+						feed = []
+						raw_feed = RSS::Parser.parse rss
+						raw_feed.items.each do |item|
+							sanitized = Sanitize.clean (item.content_encoded || item.description)
+							summary = "#{ sanitized[0...@max_summary_length] }"
+
+							feed_item = {
+								'source_title' => raw_feed.channel.title,
+								'source_url'   => raw_feed.channel.link,
+								'title'        => item.title,
+								'url'          => item.link,
+								'date'         => item.date,
+								'summary'      => summary
+							}
+
+							feed << feed_item
+						end
+						@feeds << feed
 					end
-					@feeds << feed
 				end
 			end
 
@@ -55,7 +86,7 @@ module Jekyll
 			items = []
 			feeds.each do |feed_items|
 				feed_items.each do |item|
-					if item.date < date
+					if item['date'] < date
 						items << item
 						break
 					end
@@ -66,37 +97,23 @@ module Jekyll
 				end
 			end
 
-			html = <<~HTML
-				<section class="webring">
-					<h3>Articles from blogs I follow around the net</h3>
-					<section class="articles">
-			HTML
+			site = context.registers[:site]
+			liquid_opts = site.config['liquid']
 
-			items.each do |item|
-				html += <<~HTML
-					<div class="article">
-						<h4 class="title">
-							<a href="#{ item.url }" target="_blank" rel="noopener">#{ item.title }</a>
-						</h4>
-						<p class="summary">#{ item.summary }</p>
-						<small class="source">
-							via <a href="#{ item.source_url }">#{ item.source_title }</a>
-						</small>
-						<small class="date">#{ item.date }</small>
-					</div>
-				HTML
-			end
+			template = site.liquid_renderer.file('').parse(Jekyll::Webring::TEMPLATE)
 
-			html += <<~HTML
-					</section>
-				</section>
-			HTML
+			info = {
+				:registers        => { :site => site, :page => context['page'] },
+				:strict_filters   => liquid_opts['strict_filters'],
+				:strict_variables => liquid_opts['strict_variables'],
+			}
 
-			html
+			payload = context
+			payload['webring'] = items
+
+			template.render!(payload, info)
 		end
-
 	end
-
 end
 
 Liquid::Template.register_tag('webring', Jekyll::WebringTag)
